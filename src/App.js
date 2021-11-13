dd3ae746f945370966c105eb995dfdd4f56ae201
Camelia Ben Laamari <camelia.benlaamari@supcom.tn>
2021-11-13 16:03:29 +0100

    Adding API configuration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Basic YWRtaW46YWRtaW4=");
+  myHeaders.append("Cookie", "JSESSIONID=C86FD8403E865D0D94F4CBB79C232830");
+  myHeaders.append("Content-Type", "application/json");
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow',
+  };
+
+  fetch("http://localhost:8081/jenkins/api/json", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div></div>
   );
 }
 
