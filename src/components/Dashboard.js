558bf3025cbb45b2a1e18e1acc93180922c82fee
Camelia Ben Laamari <camelia.benlaamari@supcom.tn>
2021-12-14 10:09:23 +0100

    Styling dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,11 +2,24 @@ import React from 'react'
 import "@progress/kendo-theme-material/dist/all.css";
 import { TileLayout } from "@progress/kendo-react-layout";
 import { useState } from "react";
+import { fetchBuilds } from '../jenkins';
 
 // Retrieve the Jenkins data from the browser's local storage and parse it into JSON format
 const data = JSON.parse(localStorage.getItem('userData'));
 
-const WidgetOne = () => <div>Widget one</div>;
+const WidgetOne = () => <ul className="jobs">{data["jobs"].map((job, i) =>
+    <li>
+        <div className="job-name">
+            <a href={job['url']} target="_blank" key={i} className="job-name">
+                {job['name']}&nbsp;<img src='icons/expand.png' style={{ height: "2vh" }} />
+            </a>
+        </div>
+
+        <ul className="builds">{fetchBuilds(job['name'])['builds'].reverse().map((build, j) =>
+            <li><a href={build['url']} target="_blank" key={j}>Build n°{build['number']}</a></li>
+        )}</ul>
+    </li>
+)}</ul>;
 const WidgetTwo = () => <div>{data["numExecutors"]}</div>;
 
 const initialPositions = [
@@ -28,11 +41,11 @@ function Dashboard() {
 
     const widgets = [
         {
-            header: "Widget one header",
+            header: "Jobs",
             body: <WidgetOne />,
         },
         {
-            header: "Widget two header",
+            header: "Number of Executors",
             body: <WidgetTwo />,
         },
     ];
